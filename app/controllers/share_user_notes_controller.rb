ba6644d0b71b1dea37b5aa85b3ffd37341446b81
sam <sambricking@gmail.com>
2014-05-02 18:09:52 +0000

    make shitty notes less shitty

--- app/controllers/share_user_notes_controller.rb
+++ app/controllers/share_user_notes_controller.rb
@@ -7,10 +7,12 @@ class ShareUserNotesController < ActionController::Base
       email = params[:share_user_note][:email]
       note = params[:note_id]
       if ShareUserNote.exists?(email: email, note_id: note)
-        redirect_to notes_url, :notice => "Note has already been share with this user"
-      else 
+        redirect_to notes_url, :notice => "Note no have been share. No more share."
+      elsif User.exists?(email: email)
         ShareUserNote.create(email: email, note_id: note)
-        redirect_to notes_url, :notice => "Shared note"
+        redirect_to notes_url, :notice => "Note have been share"
+      else 
+        redirect_to notes_url, :notice => "User is not exist"
       end
       
     else
